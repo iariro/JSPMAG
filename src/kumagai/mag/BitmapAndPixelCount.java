edf0faba77f59b06ee438397c7f8be34761e2208
kumagai <kumagai@toshiba-tops.co.jp>
2016-09-05 18:03:58 +0900

    new

---
+++ src/kumagai/mag/BitmapAndPixelCount.java
@@ -0,0 +1,34 @@
+package kumagai.mag;
+
+/**
+ * ビットマップ部とピクセル参照カウント
+ * @author kumagai
+ */
+public class BitmapAndPixelCount
+{
+	public final int [][] bitmap;
+	public final int pixelCount;
+	public final int referCount;
+
+	/**
+	 * ビットマップ部とピクセル参照カウント
+	 * @param bitmap ビットマップ部
+	 * @param pixelCount ピクセル新規参照カウント
+	 * @param referCount 既存ピクセル参照カウント
+	 */
+	BitmapAndPixelCount(int [][] bitmap, int pixelCount, int referCount)
+	{
+		this.bitmap = bitmap;
+		this.pixelCount = pixelCount;
+		this.referCount = referCount;
+	}
+
+	/**
+	 * ピクセル新規参照の割合を返却
+	 * @return ピクセル新規参照の割合
+	 */
+	public float getPixelCountRatio()
+	{
+		return (float)pixelCount * 100 / (float)(pixelCount + referCount);
+	}
+}
